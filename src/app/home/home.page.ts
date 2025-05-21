d885c7beb0009d95cc52f3d08fa0a488d9d717c9
AjitSingh-Brar <ajitsinghbrar0025@gmail.com>
2025-05-21 16:26:44 -0400

    UPDT: Codebase updated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { UserComponent } from "../user/user.component";
 import { SAMPLE_USERS } from '../sample-users';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, UserComponent, NgFor],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, UserComponent, NgFor, NgIf],
 })
 export class HomePage {
 
